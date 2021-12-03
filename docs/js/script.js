9a5c8d75017053d1a33c8f2b7c8fe0b5fc521e81
Kovalenko Vasyl <kovalenkova26@gmail.com>
2021-12-03 15:39:40 +0200

    add animation

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,24 +1,30 @@
-function popup() {
-    const popupButton = document.querySelector('.addToBasket'),
-        popup = document.querySelector('.popup'),
-        overlay = document.querySelector('.overlay'),
-        close = document.querySelector('.close');
-    //Окно будет отображаться лишь один раз после клика "Добавить в корзину"
-    let countClick = 1;
-    popupButton.addEventListener('click', () => {
-        let count = countClick++;
-        count < 2 ? popup.style.display = 'block' : popup.style.display = 'none';
-    });
-    /**При клике на оверлей окно скрывается, а счетчик заново обнуляется,
-    потому что пользователь мог нажать на оверлей случайно **/
-    overlay.addEventListener('click', () => {
-        popup.style.display = 'none';
-        countClick = 1;
-    });
-    //Закрытие окна
+//Закрытие окна
+function closePopup() {
+    const popup = document.querySelector('.popup'),
+    close = document.querySelector('.close');
+
     close.addEventListener('click', () => {
-        popup.style.display = 'none';
+        popup.classList.toggle("hide__popup");
     });
 }
 
-popup();
+function openPopup() {
+    const popup = document.querySelector('.popup'),
+    // overlay = document.querySelector('.overlay'),
+    popupButton = document.querySelector('.addToBasket');
+    let countClick = 1;
+    //Окно будет отображаться лишь один раз после клика "Добавить в корзину"
+    popupButton.addEventListener('click', () => {
+        let count = countClick++;
+        count < 2 ? popup.classList.toggle("show__popup") : popup.classList.toggle("hide__popup");
+    });
+    /**При клике на оверлей окно скрывается, а счетчик заново обнуляется,
+    потому что пользователь мог нажать на оверлей случайно **/
+    // overlay.addEventListener('click', () => {
+    //     popup.style.display = 'none';
+    //     countClick = 1;
+    // });
+}
+
+openPopup();
+closePopup();
