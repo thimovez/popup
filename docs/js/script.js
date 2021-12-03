99e320802227e293c35e80abf5965804fceae158
Kovalenko Vasyl <kovalenkova26@gmail.com>
2021-12-03 15:48:22 +0200

    add animation

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,10 +4,10 @@ function closePopup() {
     close = document.querySelector('.close');
 
     close.addEventListener('click', () => {
-        popup.classList.toggle("hide__popup");
+        popup.classList.toggle(".hide__popup");
     });
 }
-
+//Открытие окна
 function openPopup() {
     const popup = document.querySelector('.popup'),
     // overlay = document.querySelector('.overlay'),
@@ -16,7 +16,7 @@ function openPopup() {
     //Окно будет отображаться лишь один раз после клика "Добавить в корзину"
     popupButton.addEventListener('click', () => {
         let count = countClick++;
-        count < 2 ? popup.classList.toggle("show__popup") : popup.classList.toggle("hide__popup");
+        count < 2 ? popup.classList.toggle(".show__popup") : popup.classList.toggle(".hide__popup");
     });
     /**При клике на оверлей окно скрывается, а счетчик заново обнуляется,
     потому что пользователь мог нажать на оверлей случайно **/
@@ -25,6 +25,18 @@ function openPopup() {
     //     countClick = 1;
     // });
 }
+//Cообщение после отправки номера
+function thankYou() {
+    const sendMessage = document.querySelector('.popup__button');
+    const message = {
+        succes: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    }
+    sendMessage.addEventListener('submit', () => {
+        console.log(succes)
+    });
+}
 
 openPopup();
 closePopup();
+thankYou();
